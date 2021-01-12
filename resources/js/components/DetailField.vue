6b02528145e6a65acebb6d8fbbb9ad0c75f27a56
Maris <webstarty@gmail.com>
2021-01-12 14:42:34 +0100

    detail field

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,22 @@
+<template>
+    <panel-item :field="field">
+        <template slot="value">
+            <avatar
+                :username="field.value"
+                :rounded="field.rounded"
+                :size="field.avatar_size"
+                :src="field.image"
+                :color="field.color"></avatar>
+        </template>
+    </panel-item>
+</template>
+
+<script>
+import Avatar from 'vue-avatar'
+export default {
+    props: ['resource', 'resourceName', 'resourceId', 'field'],
+    components: {
+        Avatar
+    }
+}
+</script>
