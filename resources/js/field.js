cdd22064659827ef926a7bf0b1ab107fb4834e2c
Maris <webstarty@gmail.com>
2021-01-12 14:41:38 +0100

    added detail view

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,3 +1,4 @@
 Nova.booting((Vue, router, store) => {
-  Vue.component('index-nova-vue-avatar', require('./components/IndexField'))
+  Vue.component('index-nova-vue-avatar', require('./components/IndexField'));
+  Vue.component('detail-nova-vue-avatar', require('./components/DetailField'));
 })
