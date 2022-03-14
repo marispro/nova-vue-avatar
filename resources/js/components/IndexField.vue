d5de18571873f70cea0fb24ab55b75bad39abdc4
Maris <webstarty@gmail.com>
2022-03-14 13:29:39 +0100

    added backgroundColor

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -4,7 +4,8 @@
         :rounded="field.rounded"
         :size="field.avatar_size"
         :src="field.image"
-        :color="field.color"></avatar>
+        :color="field.color"
+        :backgroundColor="field.backgroundColor"></avatar>
 </template>
 
 <script>
