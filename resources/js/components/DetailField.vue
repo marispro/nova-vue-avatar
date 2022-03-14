dfec197721326c58506296091b97bada8f4bd034
Maris <webstarty@gmail.com>
2022-03-14 13:29:23 +0100

    added backgroundColor

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -6,7 +6,8 @@
                 :rounded="field.rounded"
                 :size="field.avatar_size"
                 :src="field.image"
-                :color="field.color"></avatar>
+                :color="field.color"
+                :backgroundColor="field.backgroundColor"></avatar>
         </template>
     </panel-item>
 </template>
